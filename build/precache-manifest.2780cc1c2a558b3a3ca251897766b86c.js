665ffe41e2b835642a9886b894c1379e0ea6b42e
Michał Skrzypczyński <justflow555@gmail.com>
2020-02-02 18:20:13 +0100

    add _redirects file to the build

---
+++ build/precache-manifest.2780cc1c2a558b3a3ca251897766b86c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d335d0a122106bd11a65edc7565c0067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90f6dddbddc8adb9a10",
+    "url": "/static/css/main.3c97e1e3.chunk.css"
+  },
+  {
+    "revision": "dcd49d5cd6147a9e2e36",
+    "url": "/static/js/2.08aaaed9.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/static/js/2.08aaaed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90f6dddbddc8adb9a10",
+    "url": "/static/js/main.5b32e3f1.chunk.js"
+  },
+  {
+    "revision": "9d971e73213ebf79f5ee",
+    "url": "/static/js/runtime-main.44b3cb40.js"
+  },
+  {
+    "revision": "69b80e2f33bbc09e429789126f4be2fe",
+    "url": "/static/media/sloth.69b80e2f.png"
+  }
+]);
